58eab7b11a56ec567eb2921cc746297093bf9be2
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-27 12:49:08 +0530

    client init

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-600 text-white text-3xl font-semibold">Hello Client here!</h1>
+    </div>
+  )
+}
+
+export default App;
